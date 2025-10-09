9c6865005094c7ae10b17a067460e172ab274ff9
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-10-09 11:54:09 -0300

    EMS-N-03-04.06 - Implementing contract-oriented resource.

---
+++ src/test/java/com/rafaelsousa/algashop/product/catalog/contract/base/ProductBase.java
@@ -0,0 +1,26 @@
+package com.rafaelsousa.algashop.product.catalog.contract.base;
+
+import com.rafaelsousa.algashop.product.catalog.presentation.ProductController;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+@WebMvcTest(controllers = ProductController.class)
+public class ProductBase {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8).build());
+
+        RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
