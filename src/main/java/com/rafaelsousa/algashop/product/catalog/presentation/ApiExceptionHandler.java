46cc9b488c0b66a0678c5893f4d3f244b850bcd1
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-10-10 15:59:58 -0300

    EMS-N-03-04.10 - Testing RFC 7807 standard error responses.

---
+++ src/main/java/com/rafaelsousa/algashop/product/catalog/presentation/ApiExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.rafaelsousa.algashop.product.catalog.presentation;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@AllArgsConstructor
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    public static final String TIMESTAMP_PROPERTY_NAME = "timestamp";
+
+    private final MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Invalid fields");
+        problemDetail.setDetail("One or more fields are invalid");
+        problemDetail.setType(URI.create("/errors/invalid-fields"));
+
+        Map<String, String> fieldErrors = ex.getBindingResult().getAllErrors().stream().collect(
+                Collectors.toMap(
+                        objectError -> ((FieldError) objectError).getField(),
+                        objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale())
+                )
+        );
+
+        problemDetail.setProperty("fields", fieldErrors);
+        problemDetail.setProperty(TIMESTAMP_PROPERTY_NAME, OffsetDateTime.now());
+
+        return super.handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+}
