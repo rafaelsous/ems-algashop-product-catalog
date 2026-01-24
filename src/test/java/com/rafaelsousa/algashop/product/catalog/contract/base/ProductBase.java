19a3f4abe3536bc84bd4c1426e894a17e590ef71
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-01-24 11:32:00 -0300

    EMS-N-04-02.04 - Including new dependencies and updating others.

--- src/test/java/com/rafaelsousa/algashop/product/catalog/contract/base/ProductBase.java
+++ src/test/java/com/rafaelsousa/algashop/product/catalog/contract/base/ProductBase.java
@@ -12,7 +12,7 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
