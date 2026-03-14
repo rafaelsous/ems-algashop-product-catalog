57e758c9e69d84aa3814e44b807d25d25b6ab2fc
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-03-14 13:08:18 -0300

    EMS-N-04-09.17 - Invalidating cache with CacheEvict annotation.

--- src/main/java/com/rafaelsousa/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
+++ src/main/java/com/rafaelsousa/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
@@ -9,6 +9,7 @@ import com.rafaelsousa.algashop.product.catalog.domain.model.product.*;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,7 +33,8 @@ public class ProductManagementApplicationService {
     }
 
 	@CachePut(cacheNames = "algashop:products:v1", key = "#result.id", condition = "#productInput.enabled == true")
-    public ProductDetailOutput update(UUID productId, ProductInput productInput) {
+	@CacheEvict(cacheNames = "algashop:products:v1", key = "#productId", condition = "#productInput.enabled == false")
+	public ProductDetailOutput update(UUID productId, ProductInput productInput) {
         Product product = findProduct(productId);
         Category category = findCategory(productInput.getCategoryId());
 
@@ -44,6 +46,7 @@ public class ProductManagementApplicationService {
 		return mapper.convert(product, ProductDetailOutput.class);
     }
 
+	@CacheEvict(cacheNames = "algashop:products:v1", key = "#productId")
     public void disable(UUID productId) {
         Product product = findProduct(productId);
         product.disable();
