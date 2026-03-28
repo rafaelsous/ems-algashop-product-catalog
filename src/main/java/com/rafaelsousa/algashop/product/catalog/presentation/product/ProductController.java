d427ffea85512fea2a8af0697d09789a974352c7
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-03-28 11:45:50 -0300

    EMS-N-04-10.14 - Implementing resilience with Spring Cloud Circuit Breaker in integration with Rapidex.

--- src/main/java/com/rafaelsousa/algashop/product/catalog/presentation/product/ProductController.java
+++ src/main/java/com/rafaelsousa/algashop/product/catalog/presentation/product/ProductController.java
@@ -42,7 +42,7 @@ public class ProductController {
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDetailOutput> findById(@PathVariable("productId") UUID productId) {
 
-		if (productId.equals(UUID.fromString("946cea3b-d11d-4f11-b88d-3089b4e74087"))) {
+		/*if (productId.equals(UUID.fromString("946cea3b-d11d-4f11-b88d-3089b4e74087"))) {
 			return ResponseEntity.badRequest().build();
 		}
 
@@ -50,7 +50,7 @@ public class ProductController {
 		    try {
 			    Thread.sleep(Duration.ofSeconds(20));
 		    } catch (Exception _) {}
-	    }
+	    }*/
 
 	    ProductDetailOutput product = productQueryService.findById(productId);
 
