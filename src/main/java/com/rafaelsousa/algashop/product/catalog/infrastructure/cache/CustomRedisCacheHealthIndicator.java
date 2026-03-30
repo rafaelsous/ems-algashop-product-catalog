1d7e9435daa1b48aad8a8185915c93d8cc1adfd7
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-03-30 20:10:12 -0300

    EMS-N-04-11.05 - Customizing Health Check.

---
+++ src/main/java/com/rafaelsousa/algashop/product/catalog/infrastructure/cache/CustomRedisCacheHealthIndicator.java
@@ -0,0 +1,29 @@
+package com.rafaelsousa.algashop.product.catalog.infrastructure.cache;
+
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.stereotype.Component;
+
+@Component("cache")
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
+public class CustomRedisCacheHealthIndicator implements HealthIndicator {
+	private final RedisConnectionFactory redisConnectionFactory;
+
+	@Override
+	public @Nullable Health health() {
+		try {
+			redisConnectionFactory.getConnection().ping();
+			return Health.up().build();
+		} catch (Exception ex) {
+			return Health.status("DEGRADED")
+					.withDetail("error", ex.getMessage())
+					.withException(ex)
+					.build();
+		}
+	}
+}
