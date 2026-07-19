e2cb7466292083573fd804e4107fcb878efa3bc3
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-07-18 22:49:37 -0300

    EMS-N-06-01.04 - Product catalog microservice as Eureka client.

---
+++ src/main/java/com/rafaelsousa/algashop/product/catalog/infrastructure/discovery/EurekaClientConfig.java
@@ -0,0 +1,8 @@
+package com.rafaelsousa.algashop.product.catalog.infrastructure.discovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {}
