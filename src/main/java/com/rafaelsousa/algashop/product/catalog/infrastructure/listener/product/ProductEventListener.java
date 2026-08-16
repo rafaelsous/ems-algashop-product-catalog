82353c2790d8c60538f9bd6eb58e98e5b3974475
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-08-16 15:38:32 -0300

    EMS-N-07-03.08 - Customizing the TypeId for serialization.

--- src/main/java/com/rafaelsousa/algashop/product/catalog/infrastructure/listener/product/ProductEventListener.java
+++ src/main/java/com/rafaelsousa/algashop/product/catalog/infrastructure/listener/product/ProductEventListener.java
@@ -1,9 +1,14 @@
 package com.rafaelsousa.algashop.product.catalog.infrastructure.listener.product;
 
 import com.rafaelsousa.algashop.product.catalog.application.IntegrationEventPublisher;
+import com.rafaelsousa.algashop.product.catalog.application.product.event.ProductDelistedIntegrationEvent;
+import com.rafaelsousa.algashop.product.catalog.application.product.event.ProductListedIntegrationEvent;
+import com.rafaelsousa.algashop.product.catalog.application.utility.Mapper;
 import com.rafaelsousa.algashop.product.catalog.domain.model.product.*;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -11,47 +16,50 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ProductEventListener {
+    private final Mapper mapper;
     private final IntegrationEventPublisher integrationEventPublisher;
 
     @EventListener(ProductPriceChangedEvent.class)
     public void handle(ProductPriceChangedEvent event) {
         log.info("ProductPriceChangedEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+//        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
     }
 
     @EventListener(ProductPlacedOnSaleEvent.class)
     public void handle(ProductPlacedOnSaleEvent event) {
         log.info("ProductPlacedOnSaleEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+//        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
     }
 
     @EventListener(ProductAddedEvent.class)
     public void handle(ProductAddedEvent event) {
         log.info("ProductAddedEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+//        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
     }
 
     @EventListener(ProductListedEvent.class)
     public void handle(ProductListedEvent event) {
         log.info("ProductListedEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+        ProductListedIntegrationEvent integrationEvent = mapper.convert(event, ProductListedIntegrationEvent.class);
+        integrationEventPublisher.send(integrationEvent, integrationEvent.getProductId().toString(), "product-catalog.product.events");
     }
 
     @EventListener(ProductDelistedEvent.class)
     public void handle(ProductDelistedEvent event) {
         log.info("ProductDelistedEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+        ProductDelistedIntegrationEvent integrationEvent = mapper.convert(event, ProductDelistedIntegrationEvent.class);
+        integrationEventPublisher.send(integrationEvent, integrationEvent.getProductId().toString(), "product-catalog.product.events");
     }
 
     @EventListener(ProductRestockedEvent.class)
     public void handle(ProductRestockedEvent event) {
         log.info("ProductRestockedEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+//        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
     }
 
     @EventListener(ProductSoldOutEvent.class)
     public void handle(ProductSoldOutEvent event) {
         log.info("ProductSoldOutEvent: {}", event);
-        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
+//        integrationEventPublisher.send(event, event.getProductId().toString(), "product-catalog.product.events");
     }
 }
